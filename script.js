8cf9e2476dd4f7c6112948e2e9f257bfcc0190ec
gachu2002 <lethanhnguyenak29@gmail.com>
2024-11-19 15:25:11 +0700

    Finish all LOGIC, havent check again, need to update hover color too

--- script.js
+++ script.js
@@ -2,25 +2,18 @@ const gameBoard = (function(doc){
     const boardElement = doc.querySelector('.board');
     const cellElements = doc.querySelectorAll('.cell');
     const cells = [];
-    const cellValues = [];
-    let playerSymbol = 'X';
-    
-    const makingMove = () => {
-        boardElement.addEventListener('click', (event) => {
-            const target = event.target;
-            if(target.getAttribute('class') === 'cell' && target.innerText === '') {
-                target.innerText = playerSymbol;
-            }
-        })
+    let playerSymbol = '';
+
+    const getPlayerSymbol = () => {
+        return playerSymbol;
     }
 
     const setPlayerSymbol = (symbol) => {
-        this.playerSymbol = symbol;
+        playerSymbol = symbol;
     } 
 
     cellElements.forEach(element => {
         cells.push(element);
-        cellValues.push(element.innerText);
     });
 
     const disableBoard = () => {
@@ -31,54 +24,56 @@ const gameBoard = (function(doc){
         boardElement.classList.remove('disable');
     }
 
+    // Check the board state - Win or Draw or Continue
     const getBoardState = () => {
         let isFull = true;
 
         for(let i = 0; i < 3; i++){
-            if(cellValues[i] === '') {
+            if(cells[i].innerText === '') {
                 isFull = false;
                 continue;   
             }
-            if(cellValues[i] === cellValues[i+3] && cellValues[i] === cellValues[i+6]) {
-                return cellValues[i];
+            if(cells[i].innerText === cells[i+3].innerText && cells[i].innerText === cells[i+6].innerText) {
+                return cells[i].innerText;
             }
         }      
         for(let i = 0; i < 9; i+=3){
-            if(cellValues[i] === '') {
+            if(cells[i].innerText === '') {
                 isFull = false;
                 continue;   
             }
-            if(cellValues[i] === cellValues[i+1] && cellValues[i] === cellValues[i+2]) {
-                return cellValues[i];
+            if(cells[i].innerText === cells[i+1].innerText && cells[i].innerText === cells[i+2].innerText) {
+                return cells[i].innerText;
             }
         }
 
-        if(cellValues[0] === cellValues[4] && cellValues[0] === cellValues[8]) 
-            return cellValues[0];
-        if(cellValues[2] === cellValues[4] && cellValues[2] === cellValues[6]) 
-            return cellValues[2];
+        if(cells[0].innerText !== '' && cells[0].innerText === cells[4].innerText && cells[0].innerText === cells[8].innerText) 
+            return cells[0].innerText;
+        if(cells[2].innerText !== '' && cells[2].innerText === cells[4].innerText && cells[2].innerText === cells[6].innerText) 
+            return cells[2].innerText;
 
         return isFull ? "Draw" : "Continue";
     }
 
+    const clearBoard = () => {
+        cells.forEach((cell) => {
+            cell.innerText = '';
+        })
+    }
+
     return {
-        makingMove,
         disableBoard,
         enableBoard,
         getBoardState,
         setPlayerSymbol,
+        getPlayerSymbol,
+        clearBoard,
     }
 })(document);
 
 const playerMenu = (function(doc){
     const menuElement = doc.querySelector('.pick-symbol');
     let playerSymbol = '';
-
-    menuElement.addEventListener('click', (event)=>{
-        let target = event.target;
-        if(target.getAttribute('id') === "X") playerSymbol = 'X';
-        else if(target.getAttribute('id') === "O") playerSymbol = "O";
-    });
     
     const getPlayerChoice = () => {
         return playerSymbol;
@@ -90,48 +85,88 @@ const playerMenu = (function(doc){
     const disableMenu = () => {
         menuElement.classList.add('disable');
     }
+
+    const restartPlayerChoice = () => {
+        playerSymbol = '';
+    }
+
     return {
         enableMenu,
         disableMenu,
         getPlayerChoice,
+        restartPlayerChoice,
     }
 })(document);
 
 const game = (function(doc){
-    const playGame = (firstPlayer, secondPlayer) => {
-        let turnNum = 0;
-        switch(gameBoard.getBoardState().toLowerCase()){
-            case 'continue':
-                turnNum++;
-                if(turnNum%2 === 0) gameBoard.setPlayerSymbol(firstPlayer);
-                else gameBoard.setPlayerSymbol(secondPlayer);
-                break;
-            case 'draw':
-                return 'DRAW';
-            case firstPlayer:
-                return "FIRST PLAYER WIN";
-            case secondPlayer:
-                return "SECOND PLAYER WIN";
-        }     
+    // Function to play the game
+    let turnNum = 0;
+    let firstPlayerSymbol = "X";
+    let secondPlayerSymbol = "O"; 
+
+    const playGame = () => {
+        doc.querySelector('.board').addEventListener('click', (event) => {
+            const target = event.target;
+            if(target.classList.contains("cell") && target.innerText === '') {
+                if(gameBoard.getBoardState().toLowerCase() === "continue"){
+                    if(turnNum % 2 === 0) gameBoard.setPlayerSymbol(firstPlayerSymbol);
+                    else gameBoard.setPlayerSymbol(secondPlayerSymbol);
+                    target.innerText = gameBoard.getPlayerSymbol();
+                    turnNum += 1;
+                    console.log(`${turnNum} ${gameBoard.getBoardState()}`);
+                }
+                switch(gameBoard.getBoardState()){
+                    case 'Draw':
+                        doc.querySelector('.result>span').innerText = 'DRAW';
+                        break;
+                    case firstPlayerSymbol:
+                        doc.querySelector('.result>span').innerText = "FIRST PLAYER WIN";
+                        break;
+                    case secondPlayerSymbol:
+                        doc.querySelector('.result>span').innerText = "SECOND PLAYER WIN";
+                        break;
+                }     
+            }
+        })
+    }
+
+    const restartGame = () => {
+        gameBoard.clearBoard();
+        gameBoard.disableBoard();
+        playerMenu.restartPlayerChoice();
+        playerMenu.enableMenu();
+        doc.querySelector('.result>span').innerText = '';
     }
 
     doc.addEventListener('DOMContentLoaded', () => {
         gameBoard.disableBoard();
         playerMenu.enableMenu();
 
-        doc.querySelector('.pick-symbol').addEventListener('click',() => {
-            if(playerMenu.getPlayerChoice() !== ''){
+        doc.querySelector('.pick-symbol').addEventListener('click',(event) => {
+            const target = event.target;
+            if(["X", "O"].includes(target.getAttribute("id"))){
+                if(target.getAttribute('id') === "X") {
+                    gameBoard.setPlayerSymbol('X');
+                    console.log("X");
+                }
+                else if(target.getAttribute('id') === "O") {
+                    gameBoard.setPlayerSymbol('O');
+                    console.log("O");
+                }
+                firstPlayerSymbol = gameBoard.getPlayerSymbol();
+                secondPlayerSymbol = firstPlayerSymbol === 'X' ? 'O' : 'X'; 
+
+                console.log(firstPlayerSymbol + " " + secondPlayerSymbol);
+                
                 gameBoard.enableBoard();
                 playerMenu.disableMenu();
+
+                playGame();
             }
         });
-
-        let firstPlayerSymbol = playerMenu.getPlayerChoice();
-        let secondPlayerSymbol = firstPlayerSymbol === 'X' ? 'O' : 'X'; 
-
-        doc.querySelector('.board').addEventListener('click', () => {
-            if(playGame(firstPlayerSymbol, secondPlayerSymbol) !== 'continue') 
-                doc.querySelector('.result>span').innerText = playGame(firstPlayerSymbol, secondPlayerSymbol);
+        doc.querySelector('#restart').addEventListener('click',() => {
+            restartGame();
+            turnNum = 0;
         })
     })
 })(document); 
